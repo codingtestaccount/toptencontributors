3186f8d30f3ac78bc092f612017b34477bb26ae2
codingtest <shaneckel+@gmail.com>
2017-09-02 14:22:40 -0400

    Added repository selection / selection component tests

--- src/components/RepositorySelect.vue
+++ src/components/RepositorySelect.vue
@@ -1,12 +1,54 @@
 <template>
-  <article class="repositorySelect">
-    <h2>Input an Organization</h2>
+  <article>
+    <h2 class="title">Input an Organization</h2>
+
+    <form v-on:submit.prevent="fetchOrganization">
+      <input v-model="organization" :placeholder="organization">
+      <input type="submit" name="button"></input>
+    </form>
+
+    <select v-if="repos" v-model="repository" v-on:change="repositorySelected">
+      <option value="">Please Select Repository.</option>
+      <option v-for="repo in repos" v-bind:value="repo.name">{{repo.name}}</option>
+    </select>
+
+    <h1>List of Contributors from {{organization}}<span v-if="repository"> / </span>{{repository}}.</h1>
+
   </article>
 </template>
 
 <script>
+import axios from 'axios'
+
 export default {
-  name: 'RespositorySelect'
+  name: 'RespositorySelect',
+  data () {
+    return {
+      repos: null,
+      organization: 'nodejs',
+      repository: '',
+      status: '',
+      statusMessage: ''
+    }
+  },
+  methods: {
+    fetchOrganization: function () {
+      axios.get(`https://api.github.com/orgs/${this.organization}/repos`)
+        .then(res => {
+          this.status = false
+          this.statusMessage = res.statusText
+          this.repos = res.data
+        })
+        .catch(error => {
+          if (error.response) {
+            this.repos = null
+            this.repository = null
+            this.status = error.response.status
+            this.statusMessage = error.response.statusText
+          }
+        })
+    } 
+  }
 }
 </script>
 
--- test/unit/specs/RepositorySelect.spec.js
+++ test/unit/specs/RepositorySelect.spec.js
@@ -1,11 +1,21 @@
-import Vue from 'vue'
 import RepositorySelect from '@/components/RepositorySelect'
+import moxios from 'moxios'
+
+import { mount } from 'avoriaz'
 
 describe('RepositorySelect.vue', () => {
   it('should render correct contents', () => {
-    const Constructor = Vue.extend(RepositorySelect)
-    const vm = new Constructor().$mount()
-    expect(vm.$el.querySelector('.repositorySelect h2').textContent)
-      .to.equal('Input an Organization')
+    const component = mount(RepositorySelect)
+    const title = component.find('h2.title')[0]
+    expect(title.text()).to.equal('Input an Organization')
   })
+
+  it('sets the organization data from the input', () => {
+    const component = mount(RepositorySelect)
+    component.setData({
+      organization: 'test'
+    })
+    expect(component.data().organization).to.contain('test')
+  })
+
 })
